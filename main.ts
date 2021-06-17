58e5e625bc6a247ecd8b0d717ecf3d1f7890c89c
moorer4002 <78602724+moorer4002@users.noreply.github.com>
2021-06-17 08:06:18 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const spike = SpriteKind.create()
+    export const face = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     newspike = sprites.create(img`
@@ -28,12 +29,50 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     jumping = false
     if (currentFrameIndex % 4 == 1) {
         currentFrameIndex = (currentFrameIndex - 1) % 16
+        playerface.setImage(frames[currentFrameIndex])
     } else {
-        while (currentFrameIndex % 4 == 0) {
-        	
+        while (currentFrameIndex % 4 != 0) {
+            currentFrameIndex = (currentFrameIndex + 1) % 16
+            playerface.setImage(frames[currentFrameIndex])
+        }
+    }
+    if (tiles.tileAtLocationEquals(location, assets.tile`myTile5`)) {
+        if (currentNormalstyleGravity) {
+            theplayer.vy = 0 - Math.sqrt(2 * (gravity * jelly_jump_hieght))
+        } else {
+            theplayer.vy = Math.sqrt(2 * (gravity * jelly_jump_hieght))
+        }
+    }
+    if (tiles.tileAtLocationEquals(location, assets.tile`myTile6`)) {
+        chnagegravity(false)
+    }
+    if (tiles.tileAtLocationEquals(location, assets.tile`myTile7`)) {
+        chnagegravity(true)
+    }
+})
+sprites.onOverlap(SpriteKind.face, SpriteKind.spike, function (sprite, otherSprite) {
+    game.over(false)
+})
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentNormalstyleGravity) {
+        if (theplayer.isHittingTile(CollisionDirection.Bottom)) {
+            theplayer.vy = 0 - Math.sqrt(2 * (gravity * jumpheight))
+        }
+    } else {
+        if (theplayer.isHittingTile(CollisionDirection.Top)) {
+            theplayer.vy = Math.sqrt(2 * (gravity * jumpheight))
         }
     }
 })
+function chnagegravity (gravityIsNormalStyle: boolean) {
+    if (gravityIsNormalStyle == currentNormalstyleGravity) {
+        return
+    } else {
+        currentNormalstyleGravity = gravityIsNormalStyle
+        theplayer.ay = 0 - theplayer.ay
+        theplayer.vy = 0
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     game.over(false)
 })
@@ -60,14 +99,23 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
     tiles.setTileAt(location, assets.tile`myTile0`)
     tiles.placeOnTile(newspike, location)
 })
+let grounded = false
 let newspike: Sprite = null
+let currentNormalstyleGravity = false
 let jumping = false
 let currentFrameIndex = 0
-let gravity = 500
+let playerface: Sprite = null
+let frames: Image[] = []
+let theplayer: Sprite = null
+let jelly_jump_hieght = 0
+let jumpheight = 0
+let gravity = 0
+gravity = 500
 let movespeed = 100
-let jelly_jump_hieght = 49
+jumpheight = 33
+jelly_jump_hieght = 49
 tiles.setTilemap(tilemap`level1`)
-let theplayer = sprites.create(img`
+theplayer = sprites.create(img`
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 
     7 1 7 7 7 7 7 7 7 7 7 7 1 7 
     7 7 7 7 7 7 7 7 7 7 7 7 7 7 
@@ -87,15 +135,41 @@ theplayer.ay = gravity
 scene.cameraFollowSprite(theplayer)
 theplayer.setVelocity(100, 0)
 theplayer.setFlag(SpriteFlag.Invisible, true)
-let frames = scaling.createRotations(theplayer.image, 16)
-theplayer.setFlag(SpriteFlag.GhostThroughWalls, true)
+frames = scaling.createRotations(theplayer.image, 16)
+playerface = sprites.create(img`
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 1 7 7 7 7 7 7 7 7 7 7 1 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 1 1 1 1 1 1 1 1 1 1 1 1 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    `, SpriteKind.face)
+playerface.setFlag(SpriteFlag.GhostThroughWalls, true)
 currentFrameIndex = 0
 jumping = false
-let currentNormalstyleGravity = true
+currentNormalstyleGravity = true
+game.onUpdate(function () {
+    if (currentNormalstyleGravity) {
+        grounded = theplayer.isHittingTile(CollisionDirection.Bottom)
+    } else {
+        grounded = theplayer.isHittingTile(CollisionDirection.Top)
+    }
+    playerface.setPosition(theplayer.x, theplayer.y)
+    if (theplayer.vx == 0) {
+        game.over(false)
+    }
+})
 game.onUpdateInterval(50, function () {
-    let grounded = 0
     if (!(grounded)) {
         currentFrameIndex = (currentFrameIndex + 1) % 16
-        theplayer.setImage(frames[currentFrameIndex])
+        playerface.setImage(frames[currentFrameIndex])
     }
 })
